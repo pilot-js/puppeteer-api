9afa2f412eb97e06a96657b7d7f5f254b7227d40
Grant Horner <h.grant.horner@gmail.com>
2019-06-13 22:20:53 -0400

    set up basic server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const puppeteer = require('puppeteer')
+
+const app = express()
+
+app.get('*', async (req, res, next) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await browser.close()
+  res.send('hello')
+});
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => console.log('listening on port 3001...'))
+
+
