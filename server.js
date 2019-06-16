b850f52a17bdd65c4b0bd7e82014f13e824f7648
Grant Horner <h.grant.horner@gmail.com>
2019-06-16 11:52:54 -0400

    added compare images

--- server.js
+++ server.js
@@ -1,68 +1,14 @@
 const express = require('express')
 const puppeteer = require('puppeteer')
-const utils = require('./puppeteer-utils')
 const path = require('path')
-const fs = require('fs')
-const { convertBufferToImgSrc } = require('./utils')
+
+const { convertBufferToImgSrc, compareImages, mkdir, parseHTML, readFile, writeFile } = require('./utils')
 
 const app = express()
 
 app.use(express.json())
 
 // helper functions 
-const mkdir = (dir) => {
-  return new Promise((res, rej) => {
-    fs.mkdir(dir, { recursive: true }, err => {
-      if (err) {
-        rej(err)
-      } else {
-        console.log('create dir: ', dir)
-        res()
-      }
-    })
-  })
-}
-
-const writeFile = (path, text) => {
-  return new Promise((res, rej) => {
-    fs.writeFile(path, text, err => {
-      if (err) {
-        rej(err)
-      } else {
-        console.log('file saved')
-        res()
-      }
-    })
-  })
-}
-
-const readFile = (path) => {
-  return new Promise((res, rej) => {
-    fs.readFile(path, (err, data) => {
-      if (err) {
-        rej(err)
-      } else {
-        console.log('file read')
-        res(data)
-      }
-    })
-  })
-}
-
-const parseHTML = (html, userId) => {
-  return `<html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Document</title>
-      <link rel="stylesheet" href="${userId}.css">
-  </head>
-  <body>
-      ${html}
-  </body>
-  </html>`
-}
 
 // /******  PREVIEW ******/
 
@@ -87,18 +33,19 @@ const createImagePreview = async (userId, dir, imageWidth, imageHeight) => {
 app.post('/create-image', async (req, res, next) => {
   try {
     const { html, css, userId, challengeId, width, height } = req.body
-    const dir = path.join(__dirname,'./tmp/')
+    const dir = path.join(__dirname, './tmp/')
+    const fileName = `${userId}-${challengeId}`
     await mkdir(dir)
-    await writeFile(`${dir}${userId}.html`, parseHTML(html, userId))
-    await writeFile(`${dir}${userId}.css`, css)
+    await writeFile(`${dir}${fileName}.html`, parseHTML(html, userId))
+    await writeFile(`${dir}${fileName}.css`, css)
     const args = ['--no-sandbox', '--disable-setuid-sandbox']
     const browser = await puppeteer.launch({ args })
     const page = await browser.newPage()
-    await page.goto(`file://${dir}${userId}.html`)
+    await page.goto(`file://${dir}${fileName}.html`)
     await page.setViewport({ width, height })
-    await page.screenshot({ path: `${dir}${userId}.png` })
+    await page.screenshot({ path: `${dir}${fileName}.png` })
     await browser.close()
-    const data = await readFile(`${dir}${userId}.png`)
+    const data = await readFile(`${dir}${fileName}.png`)
     const obj = JSON.parse(JSON.stringify(data))
     const src = convertBufferToImgSrc(obj)
     console.log(src)
@@ -114,7 +61,7 @@ const DEFAULT_HEIGHT = 337
 app.post('/seed-image', async (req, res, next) => {
   try {
     const { html, css, userId, challengeId } = req.body
-    const dir = path.join(__dirname,'./tmp/')
+    const dir = path.join(__dirname, './tmp/')
     await mkdir(dir)
     await writeFile(`${dir}${userId}.html`, parseHTML(html, userId))
     await writeFile(`${dir}${userId}.css`, css)
@@ -131,6 +78,48 @@ app.post('/seed-image', async (req, res, next) => {
   } catch (err) {
     next(err)
   }
+})
+
+app.post('/compare-images', async (req, res, next) => {
+  try {
+    const { userchallenge, challenge, width, height, userId } = req.body
+    const dir = path.join(__dirname, './tmp/')
+    await mkdir(dir)
+    // create userchallenge image
+    const userchallengeFileName = `${userId}-${userchallenge.id}`
+    await writeFile(`${dir}${userchallengeFileName}.html`, parseHTML(userchallenge.html, userId))
+    await writeFile(`${dir}${userchallengeFileName}.css`, userchallenge.css)
+    const args = ['--no-sandbox', '--disable-setuid-sandbox']
+    const browser = await puppeteer.launch({ args })
+    const userchallengePage = await browser.newPage()
+    await userchallengePage.goto(`file://${dir}${userchallengeFileName}.html`)
+    await userchallengePage.setViewport({ width, height })
+    await userchallengePage.screenshot({ path: `${dir}${userchallengeFileName}.png` })
+    // create challenge image
+    const challengeFileName = `${userId}-${challenge.id}`
+    await writeFile(`${dir}${challengeFileName}.html`, parseHTML(challenge.solutions[0].html, userId))
+    await writeFile(`${dir}${challengeFileName}.css`, challenge.solutions[0].css)
+    const challengePage = await browser.newPage()
+    await challengePage.goto(`file://${dir}${challengeFileName}.html`)
+    await challengePage.setViewport({ width, height })
+    await challengePage.screenshot({ path: `${dir}${challengeFileName}.png` })
+    await browser.close()
+    // compare images
+    const percentMatch = await compareImages(
+      `${dir}${userchallengeFileName}.png`,
+      `${dir}${challengeFileName}.png`,
+      dir,
+      width,
+      height,
+      userId);
+    const diffImageData = await readFile(`${dir}diffImage.diff.png`);
+    const data = JSON.parse(JSON.stringify(diffImageData))
+    const diffImage = convertBufferToImgSrc(data)
+    res.send({ percentMatch, src: diffImage })
+  } catch (e) {
+    next(e)
+  }
+
 })
 
 app.get('/puppy', async (req, res, next) => {
@@ -155,10 +144,8 @@ app.get('/', async (req, res, next) => {
   } catch (e) {
     res.send('did not start puppeteer')
   }
-});
+})
 
 const port = process.env.PORT || 3001
 
 app.listen(port, () => console.log('listening on port 3001...'))
-
-
--- utils.js
+++ utils.js
@@ -1,11 +1,115 @@
 const btoa = require('btoa')
+const fs = require('fs')
+const PNG = require('pngjs').PNG
+const pixelmatch = require('pixelmatch')
 
 const convertBufferToImgSrc = imageBuffer => {
-  const str = String.fromCharCode(...new Uint8Array(imageBuffer.data))
   const base64String = btoa(String.fromCharCode(...new Uint8Array(imageBuffer.data)));
   return `data:image/png;base64,${base64String}`;
 };
 
-module.exports = {
-  convertBufferToImgSrc
+// need html and css for userchallenge and challenge
+function compareImages(userchallengePath, challengePath, dir, width, height, userId) {
+  // use promise so that can return percentMatch
+  return new Promise((resolve, reject) => {
+    try {
+      let percentMatch = 0;
+      let filesRead = 0;
+
+      // eslint-disable-next-line no-inner-declarations
+      function doneReading() {
+        if (++filesRead < 2) {
+          return;
+        }
+        const diff = new PNG({ width: img1.width, height: img1.height });
+
+        // calc # of pixels different
+        const numDiffPixels = pixelmatch(img1.data, img2.data, diff.data, img1.width, img1.height, {
+          threshold: 0.1,
+        });
+        diff
+          .pack()
+          .pipe(fs.createWriteStream(`${dir}diffImage.diff.png`))
+          .on('finish', () => {
+            percentMatch = Math.round(100 * (1 - numDiffPixels / (img1.width * img1.height)));
+            resolve(percentMatch);
+          });
+      }
+      // convert images to correct format to compare pixels
+      const img1 = fs
+        .createReadStream(challengePath)
+        .pipe(new PNG())
+        .on('parsed', doneReading);
+
+      const img2 = fs
+        .createReadStream(userchallengePath)
+        .pipe(new PNG())
+        .on('parsed', doneReading);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
+
+const mkdir = (dir) => {
+  return new Promise((res, rej) => {
+    fs.mkdir(dir, { recursive: true }, err => {
+      if (err) {
+        rej(err)
+      } else {
+        console.log('create dir: ', dir)
+        res()
+      }
+    })
+  })
+}
+
+const writeFile = (path, text) => {
+  return new Promise((res, rej) => {
+    fs.writeFile(path, text, err => {
+      if (err) {
+        rej(err)
+      } else {
+        console.log('file saved')
+        res()
+      }
+    })
+  })
+}
+
+const readFile = (path) => {
+  return new Promise((res, rej) => {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        rej(err)
+      } else {
+        console.log('file read')
+        res(data)
+      }
+    })
+  })
+}
+
+const parseHTML = (html, userId) => {
+  return `<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Document</title>
+      <link rel="stylesheet" href="${userId}.css">
+  </head>
+  <body>
+      ${html}
+  </body>
+  </html>`
+}
+module.exports = {
+  convertBufferToImgSrc,
+  compareImages,
+  mkdir,
+  readFile,
+  parseHTML,
+  writeFile
+}
+
